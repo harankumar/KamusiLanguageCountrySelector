bde409939516eb4ebd3eea6e33287e76c2aa496c
Haran Kumar <hkumar8200@gmail.com>
2017-05-27 20:52:58 -0500

    Rough Skeleton

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// TODO: (long run) -- reduce dependencies
+// TODO: pull this out into a library form
+// TODO: provide utilities to convert between ISO codes and names
+
+$(document).ready(function() {
+    $("#kamusi-country, #kamusi-langname, #kamusi-userlang").select2();
+});
