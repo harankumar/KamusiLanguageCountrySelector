fa4ec01633d3d947095f0a0fbc657186295611b1
Haran Kumar <hkumar8200@gmail.com>
2017-05-27 22:13:43 -0500

    Userlang selector pretty much working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
 // TODO: (long run) -- reduce dependencies
 // TODO: pull this out into a library form
+// TODO: get stuff out of global scope (prior to publication)
 // TODO: provide utilities to convert between ISO codes and names
 
-$(document).ready(function() {
-    $("#kamusi-country, #kamusi-langname, #kamusi-userlang").select2();
+
+// avoid duplicate selector
+var userlangSelector;
+
+function loadUserLangs() {
+    // Load userlangs from the file, place them in the selector
+    $.ajax("data/userlangs/userlangs.json", {
+        success: function (resp) {
+            // Place each of the userlangs as an option in the selector
+            for (var i = 0; i < resp.length; i++) {
+                console.log(resp[i]);
+                userlangSelector.append("<option value='" +
+                    resp[i].code +
+                    "'>" +
+                    resp[i].name +
+                    "</option>");
+            }
+            userlangSelector.select2();
+            // TODO: select a default (a guess)
+            // TODO: provide some order
+        }
+        // TODO: add any other options that are needed
+        // TODO: handle errors
+    });
+}
+
+$(document).ready(function () {
+    userlangSelector = $("#kamusi-userlang");
+    loadUserLangs();
+
+    // TODO: this line won't exist when the program is all finished
+    $("#kamusi-country, #kamusi-langname").select2();
 });
